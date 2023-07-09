2d07e1af3f6d2c63bc14a1c3dd120d6c82b9b8d7
Mckeydown <mckeydown@outlook.com>
2023-07-09 18:56:47 +0300

    added P00pium collection

--- src/collections.js
+++ src/collections.js
@@ -20,6 +20,7 @@ const collections = [
   {id: "1115960635856007188", slug:"cropepe", name: "CroPepe", image: require('./images/collections/cropepe.png'), tag: "NFT" },
   {id: "915078686934458388", slug:"grind-guild", name: "The Grind Guild", image: require('./images/collections/grind-guild.png'), tag: "other" },
   {id: "1079081330182209587", slug:"cronos-towers", name: "Cronos Towers", image: require('./images/collections/cronostowers.png'), tag: "NFT" },
+  {id: "1085832552461373450", slug:"p00pium", name: "P00pium", image: require('./images/collections/P00pium.png'), tag: "NFT" },
   {id: "921711758484328458", slug:"trooprz", name: "Trooprz", image: require('./images/collections/Trooprz.png'), tag: "nft" },
   {id: "1016711439131623454", slug:"bored-candy", name: "Bored Candy", image: require('./images/collections/BoredCandy.png'), tag: "nft, gamefi" },
   {id: "969658754838720556", slug:"crogenft", name: "CrogeNFT", image: require('./images/collections/CrogeNFT.png'), tag: "nft, gamefi" },
